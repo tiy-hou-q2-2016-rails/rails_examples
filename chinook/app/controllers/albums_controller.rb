89f3f7b39abf4b5570947ef03421f8711c8e348b
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-21 12:41:47 -0500

    Move tracks to a separate method, all classes/objects

--- chinook/app/controllers/albums_controller.rb
+++ chinook/app/controllers/albums_controller.rb
@@ -8,6 +8,37 @@ class AlbumsController < ApplicationController
       @title = title
       @artist_id = artist_id
     end
+
+    def track_count
+      tracks.count
+    end
+
+    def tracks
+      # what goes here?
+
+      tracks_sql = "
+        SELECT *
+        FROM Track
+        WHERE Track.AlbumId = #{@id}
+      "
+
+      track_results = ActiveRecord::Base.connection.execute tracks_sql
+      track_results.map do |hash|
+        track = Track.new hash["TrackID"], hash["Name"], hash["AlbumId"]
+      end
+
+
+    end
+  end
+
+  class Track
+    attr_accessor :id, :album_id, :name
+
+    def initialize(id, name, album_id)
+      @id = id
+      @name = name
+      @album_id = album_id
+    end
   end
 
   def index
@@ -16,34 +47,24 @@ class AlbumsController < ApplicationController
       album = Album.new(result["AlbumId"], result["Title"], result["ArtistId"])
       album
     end
+    @albums = @albums.sort_by {|album| album.track_count}.reverse
   end
 
   def show
     album_sql = "
-        SELECT *
+        SELECT Artist.*, Album.Name as 'ArtistName'
         FROM Album
         INNER JOIN Artist ON Album.ArtistId = Artist.ArtistID
         WHERE Album.AlbumId = #{params[:id]}
     "
 
-    tracks_sql = "
-      SELECT *
-      FROM Track
-      WHERE Track.AlbumId = #{params[:id]}
-    "
-
-    @tracks = ActiveRecord::Base.connection.execute tracks_sql
 
     # select always return an array
     results = ActiveRecord::Base.connection.execute album_sql
     # get first (and likely only) item in the array
     album_hash = results.first
-    
+
     @album = Album.new(album_hash["AlbumId"], album_hash["Title"], album_hash["ArtistId"])
-    @album.artist_name = album_hash["Name"] # because we joined, Name is Artist.Name
-
-
-
-
+    @album.artist_name = album_hash["ArtistName"]
   end
 end
