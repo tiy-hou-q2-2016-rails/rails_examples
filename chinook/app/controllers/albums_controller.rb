79e9e4c197ee004ddd19bd657471b95f7025d29b
Jesse Wolgamott <jesse@comalproductions.com>
2016-06-21 12:08:33 -0500

    Move from album as a hash to an object

--- chinook/app/controllers/albums_controller.rb
+++ chinook/app/controllers/albums_controller.rb
@@ -1,6 +1,49 @@
 class AlbumsController < ApplicationController
-  
+
+  class Album
+    attr_accessor :id, :title, :artist_id, :artist_name
+
+    def initialize(id, title, artist_id)
+      @id = id
+      @title = title
+      @artist_id = artist_id
+    end
+  end
+
   def index
-    @albums = ActiveRecord::Base.connection.execute "select * from Album order by Title asc"
+    results = ActiveRecord::Base.connection.execute "select * from Album order by Title asc"
+    @albums = results.map do |result|
+      album = Album.new(result["AlbumId"], result["Title"], result["ArtistId"])
+      album
+    end
+  end
+
+  def show
+    album_sql = "
+        SELECT *
+        FROM Album
+        INNER JOIN Artist ON Album.ArtistId = Artist.ArtistID
+        WHERE Album.AlbumId = #{params[:id]}
+    "
+
+    tracks_sql = "
+      SELECT *
+      FROM Track
+      WHERE Track.AlbumId = #{params[:id]}
+    "
+
+    @tracks = ActiveRecord::Base.connection.execute tracks_sql
+
+    # select always return an array
+    results = ActiveRecord::Base.connection.execute album_sql
+    # get first (and likely only) item in the array
+    album_hash = results.first
+    
+    @album = Album.new(album_hash["AlbumId"], album_hash["Title"], album_hash["ArtistId"])
+    @album.artist_name = album_hash["Name"] # because we joined, Name is Artist.Name
+
+
+
+
   end
 end
--- chinook/config/routes.rb
+++ chinook/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get "/albums" => 'albums#index', as: :albums
+  get "/albums/:id" => 'albums#show', as: :album
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
