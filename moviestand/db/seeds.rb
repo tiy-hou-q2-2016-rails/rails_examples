ddd50e20d501a7520e1a3baa7b3fc8d41d0502f2
Jesse Wolgamott <jesse@comalproductions.com>
2016-07-05 11:51:44 -0500

    Basic view for movies and actors

---
+++ moviestand/db/seeds.rb
@@ -0,0 +1,6 @@
+
+tom = Actor.create! name: "Tom Cruise", photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Tom_Cruise_avp_2014_4.jpg/330px-Tom_Cruise_avp_2014_4.jpg"
+thandie = Actor.create! name:  "Thandie Newton", photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Thandie_Newton_2%2C_2010.jpg/330px-Thandie_Newton_2%2C_2010.jpg"
+
+mi2 = Movie.create! title: "Mission Impossible 2", year: 1999
+topgun = Movie.create! title: "Top Gun", year: 1985
